18e59cee826dc4bdc817d41bb97f325c0f9ef585
Sheridan <sdb358@stern.nyu.edu>
2019-06-25 19:10:11 -0400

    randomize animal parsed response

--- pets.py
+++ pets.py
@@ -3,6 +3,7 @@ import os
 import datetime
 from dotenv import load_dotenv
 import requests
+import random
 
 load_dotenv()
 
@@ -15,13 +16,12 @@ headers = {"Authorization": f"Bearer {TOKEN}"}
 
 def get_response(postcode):
     request_url = f"https://api.petfinder.com/v2/animals"
-    response = requests.get(request_url, headers=headers)
+    response = requests.get(request_url, headers=headers, params={'location': postcode})
     parsed_response = json.loads(response.text)
     return parsed_response
 
 def transform_response(parsed_response):
     location= parsed_response["animals"][0]["contact"]["address"]["postcode"]
-
 #print(type(response)) #>
 #print(response.status_code)
 #pprint(response.text)
@@ -44,6 +44,21 @@ print("Welcome to Get-A-Pet")
 print("---------------------------------")
 postcode = input ("Please input your zip code to find pets in your area: ")
 parsed_response = get_response(postcode)
+
+num_animals = len(parsed_response["animals"])
+my_random_animal = parsed_response["animals"][random.randint(0, num_animals)]
+print("type: " + my_random_animal["type"]);
+
+#found_animal = None
+#for animal in parsed_response["animals"]:
+   # animal_postcode = animal["contact"]["address"]["postcode"]
+   # print(animal_postcode)
+   # if animal_postcode == postcode:
+      #  found_animal = animal
+      #  break
+        
+#print(found_animal)
+        
 
     
 
