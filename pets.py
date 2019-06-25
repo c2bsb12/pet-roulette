553773da1f14c8b1a3169510680dd76e54be826b
Sheridan <sdb358@stern.nyu.edu>
2019-06-24 22:08:31 -0400

    updated token

--- pets.py
+++ pets.py
@@ -1,28 +1,69 @@
 import json
 import os
+import datetime
 from dotenv import load_dotenv
 import requests
 
 load_dotenv()
 
 TOKEN = os.environ.get("PETFINDER_ACCESS_TOKEN")
+#API_KEY = os.environ.get('API_KEY')
 
 request_url = "https://api.petfinder.com/v2/animals"
 
 headers = {"Authorization": f"Bearer {TOKEN}"}
 
-response = requests.get(request_url, headers=headers)
+def get_response(postcode):
+    request_url = f"https://api.petfinder.com/v2/animals"
+    response = requests.get(request_url, headers=headers)
+    parsed_response = json.loads(response.text)
+    return parsed_response
+
+def transform_response(parsed_response):
+    location= parsed_response["animals"][0]["contact"]["address"]["postcode"]
+
 #print(type(response)) #>
 #print(response.status_code)
 #pprint(response.text)
 
-#parsed_response = json.loads(response.text)
+
 #print(parsed_response.keys())
 
-key = os.environ.get('API_KEY')
+#parsed_response["animals"][0]["contact"]["address"]["postcode"]
 
 
-#pf = Petfinder(key)
 
-#cats = pf.breed_list('cat')
-#print(cats)
+#breakpoint()
+#exit()
+
+#INPUTS
+
+time_now = datetime.datetime.now() #> datetime.datetime(2019, 3, 3, 14, 44, 57, 139564)
+
+print("Welcome to Get-A-Pet")
+print("---------------------------------")
+postcode = input ("Please input your zip code to find pets in your area: ")
+parsed_response = get_response(postcode)
+
+    
+
+#OUTPUTS
+
+formatted_time_now = time_now.strftime("%Y-%m-%d %H:%M:%S") #> '2019-03-03 14:45:27'
+
+
+#DISPLAY RESULTS
+
+
+print("Searching for pets in your area...")
+print("---------------------------------")
+print(f"REQUEST AT: {formatted_time_now}")
+print("---------------------------------")
+print(f"Pet Location: {postcode}")
+print("---------------------------------")
+print("Your future best friend: ")
+print("---------------------------------")
+print("Nearest adoption center: ")
+print("---------------------------------")
+print("Thank you for using Get-A-Pet!")
+   
